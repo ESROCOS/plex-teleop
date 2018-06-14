ac1013715ad30c82728d0cf86127a018d3c68baf
Benjamin <benjamin.kisliuk@dfki.de>
2018-06-14 16:05:43 +0000

    added mc receive response for testing

--- dummy_consumer/dummy_consumer.cc
+++ dummy_consumer/dummy_consumer.cc
@@ -1,6 +1,10 @@
 /* User code: This file will not be overwritten by TASTE. */
 
 #include "dummy_consumer.h"
+#include <ncurses.h>
+#include <base_support/baseTypes.h>
+#include <base_support/Base-commands-Motion2DConvert.hpp>
+
 
 void dummy_consumer_startup()
 {
@@ -10,6 +14,10 @@ void dummy_consumer_startup()
 
 void dummy_consumer_PI_consume_mc(const asn1SccBase_commands_Motion2D *IN_mc_command)
 {
-    /* Write your code here! */
+  base::commands::Motion2D base_mc;
+
+  asn1SccBase_commands_Motion2D_fromAsn1(base_mc,*IN_mc_command);      
+
+  printw("got command: t= %f  r=%f",base_mc.translation,base_mc.rotation);
 }
 
